35b65c7dd0a0f3d585c66dac03e37d342e4fbb27
samm002 <samuelsdjodis@gmail.com>
2024-11-18 09:15:54 +0000

    feat: adding option to use service account for firestore and adjusting allowed host

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,6 +1,9 @@
 const { Firestore } = require('@google-cloud/firestore');
+const path = require('path');
 
 async function storeData(id, data) {
+  // if using service account file
+  // const db = new Firestore({ keyFilename: path.resolve(__dirname, '../../firestore-service-account.json'), });
   const db = new Firestore();
 
   const predictCollection = db.collection('prediction');
